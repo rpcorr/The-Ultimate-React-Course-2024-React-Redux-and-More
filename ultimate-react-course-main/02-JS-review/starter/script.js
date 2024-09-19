a676d2c580b88509deeaedb275dd7a75618d12b1
Ronan Corr <ronan_corr@hotmail.com>
2024-09-19 13:28:03 -0400

    Ternaries instead of if/else statements

--- ultimate-react-course-main/02-JS-review/starter/script.js
+++ ultimate-react-course-main/02-JS-review/starter/script.js
@@ -187,5 +187,10 @@ updatedBook;
 // 03 - TEMPLATE LITERALS
 const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
   publicationDate.split('-')[0]
-}`;
+}. The book has${hasMovieAdaptation ? '' : ' not'} been adapted as a movie.`;
 summary;
+
+// 04 - TERNARIES INSTEAD OF IF/ELSE STATEMENTS
+const pagesRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
+pagesRange;
+console.log(`${title} has ${pagesRange} pages.`);
