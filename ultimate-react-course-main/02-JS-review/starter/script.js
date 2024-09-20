b68ec50d60adcd97c84c90bcc3b4935bc40ae036
Ronan Corr <ronan_corr@hotmail.com>
2024-09-20 05:20:14 -0400

    Working with Immutable Arrays

--- ultimate-react-course-main/02-JS-review/starter/script.js
+++ ultimate-react-course-main/02-JS-review/starter/script.js
@@ -300,3 +300,25 @@ arr;
 
 const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
 sortedByPages;
+
+// 12- Working with Immutable arrays
+
+// 1) Add book object to array
+const newBook = {
+  id: 6,
+  title: 'Harry Potter and the Chamber of Secrets',
+  author: 'J.K. Rowling',
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3) Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1210 } : book
+);
+booksAfterUpdate;
