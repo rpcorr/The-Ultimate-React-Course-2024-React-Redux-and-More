a83fa7bc969c89f410727db7dbf96fbf062518e5
Ronan Corr <ronan_corr@hotmail.com>
2024-09-29 21:58:24 -0400

    extracting JSX into a new component

--- ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
+++ ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
@@ -135,10 +135,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're closed, but are happy to welcome you between {openHour}:00 and{' '}
@@ -149,6 +146,17 @@ function Footer() {
   );
 
   //return React.createElement('footer', null, "We're currently open!");
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 // React version 18 - how React is rendered
