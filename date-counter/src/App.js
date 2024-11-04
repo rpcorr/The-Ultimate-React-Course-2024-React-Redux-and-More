a74940c249a04f4b130216b5e60157f6a64d9c10
Ronan Corr <ronan_corr@hotmail.com>
2024-11-04 14:18:17 -0500

    challenge #1: date counter (v1)

---
+++ date-counter/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const today = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  today.setDate(today.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{today.toLocaleDateString('en-US', options)}</span>
+      </p>
+    </div>
+  );
+}
