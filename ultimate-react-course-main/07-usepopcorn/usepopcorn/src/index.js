4c191951eaa3f3f6dfc08d1efef6f1c3f1cb000a
Ronan Corr <ronan_corr@hotmail.com>
2025-03-30 19:56:34 -0400

    useEffect to the rescue

--- ultimate-react-course-main/07-usepopcorn/usepopcorn/src/index.js
+++ ultimate-react-course-main/07-usepopcorn/usepopcorn/src/index.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App';
+//import StarRating from './StarRating';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating colour="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating colour="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
     />
     <StarRating size={24} colour="red" className="test" defaultRating={3} />
 
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
