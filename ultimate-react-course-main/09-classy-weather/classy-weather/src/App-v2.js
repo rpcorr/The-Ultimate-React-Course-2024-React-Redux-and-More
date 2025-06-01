39992fe4ac1daadd31e9081f7531a8e0d013173c
Ronan Corr <ronan_corr@hotmail.com>
2025-06-01 09:33:13 -0400

    lifecycle methods

--- ultimate-react-course-main/09-classy-weather/classy-weather/src/App-v2.js
+++ ultimate-react-course-main/09-classy-weather/classy-weather/src/App-v2.js
@@ -36,15 +36,13 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: '',
+    location: 'Toronto',
     isLoading: false,
     displayLocation: '',
     weather: {},
   };
 
   fetchWeather = async () => {
-    if (this.state.location.length < 2) return this.setState({ weather: {} });
-
     try {
       this.setState({ isLoading: true });
 
@@ -70,29 +68,13 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.error(err);
+      console.err(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
-
-  // similar to useEffect []
-  componentDidMount() {
-    // this.fetchWeather();
-
-    this.setState({ location: localStorage.getItem('location') || '' });
-  }
-
-  // similar to useEffect [location]
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.location !== prevState.location) {
-      this.fetchWeather();
-
-      localStorage.setItem('location', this.state.location);
-    }
-  }
 
   render() {
     return (
@@ -102,6 +84,7 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
+        <button onClick={this.fetchWeather}>Get weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
         {this.state.weather.weathercode && (
           <Weather
@@ -132,10 +115,6 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
-  componentWillUnmount() {
-    console.log('Weather will unmount');
-  }
-
   render() {
     console.log(this.props);
     const {
