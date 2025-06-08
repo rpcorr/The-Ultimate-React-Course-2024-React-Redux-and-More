1213eea275449c58a310caa4ea163ebde85d67e1
Ronan Corr <ronan_corr@hotmail.com>
2025-06-08 09:32:14 -0400

    restarting a quiz

--- ultimate-react-course-main/10-react-quiz/react-quiz/src/components/App.js
+++ ultimate-react-course-main/10-react-quiz/react-quiz/src/components/App.js
@@ -59,6 +59,20 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case 'restart':
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: 'ready',
+        highscore: state.highscore,
+      };
+    // return {
+    //   ...state,
+    //   status: 'ready',
+    //   index: 0,
+    //   answer: null,
+    //   points: 0,
+    // };
 
     default:
       throw new Error('Action unknown');
@@ -118,6 +132,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
