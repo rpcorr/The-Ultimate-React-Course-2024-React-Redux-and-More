20e3abc971117974cd9e113bfe866b5d4597a0dd
Ronan Corr <ronan_corr@hotmail.com>
2025-06-29 14:08:41 -0400

    building the app layout

---
+++ ultimate-react-course-main/11-worldwise/worldwise/src/components/Map.jsx
@@ -0,0 +1,11 @@
+import styles from './Map.module.css';
+
+function Map() {
+  return (
+    <div className={styles.mapContainer}>
+      <p>Map goes here</p>
+    </div>
+  );
+}
+
+export default Map;
