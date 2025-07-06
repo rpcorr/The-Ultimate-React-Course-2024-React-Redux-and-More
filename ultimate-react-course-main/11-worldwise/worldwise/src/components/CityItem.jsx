51c5c05d25aab49993a1685337e338a876c155e1
Ronan Corr <ronan_corr@hotmail.com>
2025-07-06 18:47:45 -0400

    have emoji flags render in Chrome and Edge

--- ultimate-react-course-main/11-worldwise/worldwise/src/components/CityItem.jsx
+++ ultimate-react-course-main/11-worldwise/worldwise/src/components/CityItem.jsx
@@ -8,6 +8,15 @@ const formatDate = (date) =>
     year: 'numeric',
   }).format(new Date(date));
 
+const flagemojiToPNG = (flag) => {
+  const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join('');
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
 
@@ -17,7 +26,7 @@ function CityItem({ city }) {
         className={styles.cityItem}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
-        <span className={styles.emoji}>{emoji}</span>
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
