62b7e19a21148ba1a7146ed8da6c4609f6f7f1ee
Ronan Corr <ronan_corr@hotmail.com>
2025-07-06 12:51:35 -0400

    reading and setting a query string

--- ultimate-react-course-main/11-worldwise/worldwise/src/components/Map.jsx
+++ ultimate-react-course-main/11-worldwise/worldwise/src/components/Map.jsx
@@ -1,9 +1,17 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
   return (
     <div className={styles.mapContainer}>
-      <p>Map goes here</p>
+      <h1>Map</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
     </div>
   );
 }
