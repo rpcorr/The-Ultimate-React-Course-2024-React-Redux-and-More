34bc2b0d4d0a723d44cab2a30dc2f5ca8cadcc88
Ronan Corr <ronan_corr@hotmail.com>
2025-12-14 22:06:53 -0500

    a surpising optimization trick with children

--- ultimate-react-course-main/12-atomic-blog/atomic-blog/src/App.js
+++ ultimate-react-course-main/12-atomic-blog/atomic-blog/src/App.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useContext, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePosts } from './PostContext';
+import Test from './Test';
 
 function createRandomPost() {
   return {
@@ -125,14 +126,18 @@ function FormAddPost() {
 function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      {/* <Test /> */}
+    </>
   );
 }
 
