84fdd79a13083c8f921fd1dec77dd1ceb0b19838
Ronan Corr <ronan_corr@hotmail.com>
2026-01-14 14:08:50 -0500

    add useCallback into the WorldWise App

--- ultimate-react-course-main/11-worldwise/worldwise/src/contexts/CitiesContext.jsx
+++ ultimate-react-course-main/11-worldwise/worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useContext, useReducer } from 'react';
+import { createContext, useEffect, useContext, useReducer, useCallback } from 'react';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -82,7 +82,7 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
+  const getCity = useCallback(async function getCity(id) {
     if (Number(id) === currentCity.id) return;
 
     dispatch({ type: 'loading' });
@@ -97,7 +97,7 @@ function CitiesProvider({ children }) {
         payload: 'There was an error loading the city...',
       });
     }
-  }
+  }, [currentCity.id] );
 
   async function createCity(newCity) {
     dispatch({ type: 'loading' });
