3761ec046e4c29d0d132609ff36cacfbd57c6092
Ronan Corr <ronan_corr@hotmail.com>
2026-01-19 11:26:16 -0500

    professional redux file structure: state slices

--- ultimate-react-course-main/15-redux-intro/redux-intro/src/index.js
+++ ultimate-react-course-main/15-redux-intro/redux-intro/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import './store';
+import store from './store';
+import { deposit } from './features/accounts/accountSlice';
+
+store.dispatch({ type: 'account/deposit', payload: 250 });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
