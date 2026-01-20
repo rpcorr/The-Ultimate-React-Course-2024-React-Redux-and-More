f8bbbf9b14b1ae6b76978b7715139ab06aed8957
Ronan Corr <ronan_corr@hotmail.com>
2026-01-20 13:31:50 -0500

    connecting Redux App with React

--- ultimate-react-course-main/15-redux-intro/redux-intro/src/features/customers/Customer.js
+++ ultimate-react-course-main/15-redux-intro/redux-intro/src/features/customers/Customer.js
@@ -1,5 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  const customer = useSelector((store) => store.customer.fullName);
+
+  console.log(customer);
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
--- ultimate-react-course-main/15-redux-intro/redux-intro/src/index.js
+++ ultimate-react-course-main/15-redux-intro/redux-intro/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
 import store from './store';
-import { deposit } from './features/accounts/accountSlice';
 
 store.dispatch({ type: 'account/deposit', payload: 250 });
 console.log(store.getState());
@@ -12,6 +12,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
