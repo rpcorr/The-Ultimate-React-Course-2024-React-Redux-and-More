0d29f1a3c6ef02a8c8e6f867f5fdd4d98ccfd54c
Ronan Corr <ronan_corr@hotmail.com>
2026-03-07 12:52:43 -0500

    using Tailwind Flexbox

--- ultimate-react-course-main/16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
+++ ultimate-react-course-main/16-fast-react-pizza/fast-react-pizza/src/features/user/Username.jsx
@@ -1,6 +1,6 @@
 function Username() {
     return (
-        <div className="hidden text-sm font-semibold">
+        <div className="hidden text-sm font-semibold md:block">
             Ronan
         </div>
     )
