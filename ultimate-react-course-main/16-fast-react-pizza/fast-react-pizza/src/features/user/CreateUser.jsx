8d36bbdba73647eb19c97dc3c0a019f552c6d4c1
Ronan Corr <ronan_corr@hotmail.com>
2026-03-07 12:41:55 -0500

    responsive design

--- ultimate-react-course-main/16-fast-react-pizza/fast-react-pizza/src/features/user/CreateUser.jsx
+++ ultimate-react-course-main/16-fast-react-pizza/fast-react-pizza/src/features/user/CreateUser.jsx
@@ -9,13 +9,13 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className='mb-4 text-sm text-stone-600 md:text-base'>👋 Welcome! Please start by telling us your name:</p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        onChange={(e) => setUsername(e.target.value)} className="w-72"
       />
 
       {username !== '' && (
