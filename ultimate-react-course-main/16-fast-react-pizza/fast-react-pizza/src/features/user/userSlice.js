af38e86f5ab819a7e797b7c5aa9fdd047b50172f
Ronan Corr <ronan_corr@hotmail.com>
2026-03-24 20:04:51 -0400

    set username value to blank

--- ultimate-react-course-main/16-fast-react-pizza/fast-react-pizza/src/features/user/userSlice.js
+++ ultimate-react-course-main/16-fast-react-pizza/fast-react-pizza/src/features/user/userSlice.js
@@ -25,7 +25,7 @@ async function fetchAddress() {
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  username: 'Rooonnnaaaannn',
+  username: '',
 };
 
 const userSlice = createSlice({
