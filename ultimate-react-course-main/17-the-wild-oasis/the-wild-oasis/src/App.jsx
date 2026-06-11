5f3e61be96e2933a157214d6aa3b1e1d0b1a89eb
Ronan Corr <ronan_corr@hotmail.com>
2026-06-11 07:09:29 -0400

    building the app layout

--- ultimate-react-course-main/17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ ultimate-react-course-main/17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -1,5 +1,6 @@
 import GlobalStyles from './styles/GlobalStyles';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -15,13 +16,16 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
