c0f5a0636ab061318856456a7fc5d1c8fb8cb3ef
Ronan Corr <ronan_corr@hotmail.com>
2026-06-16 21:42:42 -0400

    uploading images to Supabase

--- ultimate-react-course-main/17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
+++ ultimate-react-course-main/17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gonemrwsjaehshcexylf.supabase.co';
+export const supabaseUrl = 'https://gonemrwsjaehshcexylf.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvbmVtcndzamFlaHNoY2V4eWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNzYzNTcsImV4cCI6MjA5Njc1MjM1N30.CfW1tAiURyA_xnVjhE-z90BbpHlHtpuBJSHONoW0WbY';
 const supabase = createClient(supabaseUrl, supabaseKey);
